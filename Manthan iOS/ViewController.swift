e335983a9a33b7afad2fd3a20880b1c111126b0b
Manthan <manthan@Manthans-Mac-mini.local>
2025-06-03 17:00:20 +0530

    Inital Commit

---
+++ Manthan iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Manthan iOS
+//
+//  Created by Manthan on 03/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
